9239ec3d6ce5899361704e7d9d9df3d44b8115a3
brandon <bsbrionesos@gmail.com>
2021-02-27 20:47:36 -0600

    added a delete button to the table rows

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,9 +6,12 @@ class Movies extends Component {
     movies: getMovies()
   };
 
-  // handleDelete = movie => {
+  handleDelete = movie => {
+    // getting all the movies except for the movie that has been deleted
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies});
 
-  // }
+  }
 
   render() {
     
@@ -19,14 +22,16 @@ class Movies extends Component {
           <th>Genre</th>
           <th>Stock</th>
           <th>Rate</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
-        {this.state.movies.map(movie => <tr>
+        {this.state.movies.map(movie => <tr key={movie._id}>
           <td>{movie.title}</td>
           <td>{movie.genre.name}</td>
           <td>{movie.numberInStock}</td>
           <td>{movie.dailyRentalRate}</td>
+          <td><button onClick={() => this.handleDelete(movie)}className="btn btn-danger btn-sm">Delete</button></td>
         </tr>)}
       </tbody>
     </table>  
