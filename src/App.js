b0159f381109f7ebee573e77c7085830f3c2691f
brandon <bsbrionesos@gmail.com>
2021-03-13 17:00:34 -0600

    linked to movie Form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
+import MovieForm from "./components/movieForm"
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from './components/navBar';
 import "./App.css";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <NavBar/>
     <main className="container">
       <Switch>
+        <Route path="/movies/:id" component={MovieForm}></Route>
         <Route path="/movies" component={Movies}></Route>
         <Route path="/customers" component={Customers}></Route>
         <Route path="rentals" component={Rentals}></Route>
