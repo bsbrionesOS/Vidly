224be5a9ae015316e5422a3572d403a281737187
brandon <bsbrionesos@gmail.com>
2021-03-13 21:13:54 -0600

    added handleChange to username input field

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+
+  state = {
+    account: {
+      username: "",
+      password: ""
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("submitted");
+  }
+
+  handleChange = e => {
+    const account = {...this.state.account};
+
+    account.username = e.currentTarget.value 
+    this.setState({account})
+  }
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">UserName</label>
-            <input id="username" type="text" className="form-control" />
+            <input 
+            value={this.state.account.username} 
+            onChange={this.handleChange}
+            id="username" 
+            type="text" 
+            className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
