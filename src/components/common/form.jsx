891881da91f1538797ad481d338f10020deff144
brandon <bsbrionesos@gmail.com>
2021-03-16 11:39:51 -0500

    extracted helper rendering methods

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import Joi from 'joi-browser';
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from './input';
 
 class Form extends Component {
-  state = { 
+  state = {
     data: {},
-    errors:{}
-   };
+    errors: {},
+  };
 
-   validate = () => {
+  validate = () => {
     const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.data, this.schema, options);
     if (!error) return null;
@@ -20,8 +21,8 @@ class Form extends Component {
 
   validateProperty = ({ name, value }) => {
     const obj = { [name]: value };
-    const schema = { [name]: this.schema[name]};
-    const {error} = Joi.validate(obj, schema);
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
     return error ? error.details[0].message : null;
   };
 
@@ -30,7 +31,7 @@ class Form extends Component {
     const errors = this.validate();
     this.setState({ errors: errors || {} });
     if (errors) return;
-    
+
     this.doSubmit();
   };
 
@@ -46,6 +47,26 @@ class Form extends Component {
     this.setState({ data, errors });
   };
 
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label) {
+    const { data, errors } = this.state;
+    return (
+      <Input
+      name={name}
+      value={data[name]}
+      label={label}
+      onChange={this.handleChange}
+      error={errors[name]}
+    />
+    )
+  }
 }
- 
+
 export default Form;
