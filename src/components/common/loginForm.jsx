d7dba42af1c81514cd54d1ecb4d3689ed36828ac
brandon <bsbrionesos@gmail.com>
2021-03-16 01:48:38 -0500

    implemented schema for joi validation

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -8,52 +8,45 @@ class LoginForm extends Component {
       username: "",
       password: "",
     },
-    errors: {}
+    errors: {},
   };
 
   schema = {
-    username: Joi.stringify().required(),
-    password: Joi.stringify().required()
-  }
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.state.schema, { abortEarly: false});
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
 
     const errors = {};
 
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
-  validateProperty = ({name, value}) => {
-    if (name === 'username') {
-      if (value.trim() === '') return "Username is requiered.";
-    }
-    if (name === 'password') {
-      if (value.trim() === '') return "Password is requiered.";
-    }
-
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name]};
+    const {error} = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
-    this.setState( {errors: errors || {}});
+    this.setState({ errors: errors || {} });
     if (errors) return;
     console.log("submitted");
   };
 
   handleChange = ({ currentTarget: input }) => {
-    const errors = {...this.state.errors};
+    const errors = { ...this.state.errors };
     const errorMessage = this.validateProperty(input);
     if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name]
+    else delete errors[input.name];
 
     const account = { ...this.state.account };
 
@@ -74,7 +67,7 @@ class LoginForm extends Component {
             label="Username"
             onChange={this.handleChange}
             error={errors.username}
-            />
+          />
           <Input
             name="password"
             value={account.password}
